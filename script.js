1a57e6bb736662eece941e5f1b3b6b794d51ec9b
father-lox <69652031+father-lox@users.noreply.github.com>
2021-04-14 23:12:12 +0300

    Calculator v1.0

---
+++ script.js
@@ -0,0 +1,42 @@
+let display = document.querySelector(".display input[type='text']");
+let clear = document.getElementById("clear");
+let backspace = document.getElementById("backspace");
+let equal = document.querySelector(".button[special]");
+let buttons = document.querySelectorAll(".control-panel .button:not(.button[special])");
+
+function backspaceButtonLogic() {
+    let string = display.value;
+    if (display.selectionStart > 0 && display.selectionStart == display.selectionEnd) {
+        string = string.slice(0, display.selectionStart - 1) + string.slice(display.selectionStart, string.length);
+        let cursorPosition = display.selectionStart - 1;
+        display.value = string;
+        
+        if (cursorPosition == 0) {
+            display.selectionStart = display.value.length;
+        }
+        else {
+            display.selectionStart = display.selectionEnd = cursorPosition;
+        }
+    }
+    else if (display.selectionStart != display.selectionEnd) {;
+        display.setRangeText("");
+    }
+}
+
+function clearButtonLogic() {
+    display.value = "";
+}
+
+clear.addEventListener("click", clearButtonLogic);
+
+backspace.addEventListener("click", backspaceButtonLogic);
+
+equal.addEventListener("click", function() {
+    display.value = math.evaluate(display.value);
+});
+
+buttons.forEach((button) => {
+    button.addEventListener("click", function(evt) {
+        display.value += evt.target.getAttribute("data-operation");
+    });
+});
