f15e1f874dae2358443f41b02686446960347862
father-lox <kot.mi.mi.992@gmail.com>
2021-07-06 22:50:31 +0300

    Add round result

--- script.js
+++ script.js
@@ -6,67 +6,6 @@ let buttons = document.querySelectorAll(".control-panel .button:not([special])")
 let history = document.querySelector(".history");
 let hasEqual = false;
 
-function HistoryValueSetToDisplay(evt) {
-    display.value = evt.target.textContent;
-}
-
-function addToBuffer(evt) {
-    
-}
-
-function createExceptionItem(expression, result) {
-    let historyItem = document.createElement("div");
-    historyItem.classList.add("history-item");
-    historyItem.innerHTML = (`<span class="expression">${expression}</span>
-    <span class="result">${result}</span>`);
-
-    console.log(historyItem.children);
-    
-    for(let i = 0; i < historyItem.children.length; i++)
-    {
-        historyItem.children[i].addEventListener("click", HistoryValueSetToDisplay);
-    }
-    // .forEach(function(item) {
-    //     item.addEventListener("click", HistoryValueSetToDisplay);
-    // });
-    
-
-    // let expressionItem = document.createElement("span");
-    // expressionItem.classList.add("expression"), expressionItem.textContent = expression;
-    // let resultItem = document.createElement("span");
-    // resultItem.classList.add("result"), resultItem.textContent = result;
-    
-    // historyItem.appendChild(expressionItem);
-    // historyItem.appendChild(resultItem);
-    
-    return historyItem;
-}
-
-function backspaceButtonLogic() {
-    // let string = display.value;
-    // if (display.selectionStart > 0 && display.selectionStart == display.selectionEnd) {
-    //     string = string.slice(0, display.selectionStart - 1) + string.slice(display.selectionStart, string.length);
-    //     let cursorPosition = display.selectionStart - 1;
-    //     display.value = string;
-        
-    //     if (cursorPosition == 0) {
-    //         display.selectionStart = display.value.length;
-    //     }
-    //     else {
-    //         display.selectionStart = display.selectionEnd = cursorPosition;
-    //     }
-    // }
-    // else if (display.selectionStart != display.selectionEnd) {;
-    //     display.setRangeText("");
-    // }
-
-    display.value = display.value.slice(0, display.value.length - 1);
-}
-
-function clearButtonLogic() {
-    display.value = "";
-}
-
 clear.addEventListener("click", clearButtonLogic);
 
 backspace.addEventListener("click", backspaceButtonLogic);
@@ -74,7 +13,7 @@ backspace.addEventListener("click", backspaceButtonLogic);
 equal.addEventListener("click", function() {
     try {
         let expression = display.value;
-        display.value = math.evaluate(display.value);
+        display.value = math.round(math.evaluate(display.value), 3);
         let result = display.value;
         history.prepend(createExceptionItem(expression, result));
 
@@ -97,3 +36,31 @@ buttons.forEach((button) => {
         
     });
 });
+
+function HistoryValueSetToDisplay(evt) {
+    display.value = evt.target.textContent;
+}
+
+function createExceptionItem(expression, result) {
+    let historyItem = document.createElement("div");
+    historyItem.classList.add("history-item");
+    historyItem.innerHTML = (`<span class="expression">${expression}</span>
+    <span class="result">${result}</span>`);
+
+    console.log(historyItem.children);
+    
+    for(let i = 0; i < historyItem.children.length; i++)
+    {
+        historyItem.children[i].addEventListener("click", HistoryValueSetToDisplay);
+    }
+
+    return historyItem;
+}
+
+function backspaceButtonLogic() {
+    display.value = display.value.slice(0, display.value.length - 1);
+}
+
+function clearButtonLogic() {
+    display.value = "";
+}
